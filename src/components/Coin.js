4eef0c2815167112f698a902ad57ca1a6133aa59
ariesfire1244 <64576604+ariesfire1244@users.noreply.github.com>
2023-01-08 19:59:48 +0530

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -5,10 +5,10 @@ import React from "react";
 export function Coin ({name, icon, price, symbol}){
     return(
         <div className="coin">
-            <h1>Name: {name}</h1>;
-            <img src={icon} />;
-            <h1>Price: {name}</h1>;
-            <h1>Symbol: {name}</h1>;
+            <h1>Name: {name}</h1>
+            <img src={icon} />
+            <h1>Price: {price} USD</h1>
+            <h1>Symbol: {symbol}</h1>
         </div>
     )
 }
